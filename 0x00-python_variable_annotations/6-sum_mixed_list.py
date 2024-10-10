69f118176fa42a3b121510f4eda4ae4e35fef15b
fuadhasen <fuadhas6634@gmail.com>
2024-10-10 12:18:16 +0530

    add task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""module for mixed list"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """function that compute problem"""
+    return sum(mxd_lst)
