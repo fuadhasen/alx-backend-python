6f48b4b989158eff3269abdcb8715281e8b9268b
fuadhasen <fuadhas6634@gmail.com>
2024-10-14 12:04:23 +0530

    update task 0-basic async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """scripts for basics of async"""
+import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
     """asyncronus function"""
     wait_time = random.uniform(0, max_delay)
+    await asyncio.sleep(wait_time)
     return wait_time
